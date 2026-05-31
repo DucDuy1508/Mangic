0ff91b1acdd80eb6e0537b5f9b0312e55486d6e4
DucDuy1508 <ducduy150806@gmail.com>
2026-06-01 01:45:39 +0700

    hdh2

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const bcrypt = require('bcryptjs');
 
-const app = Web = express();
+const app = express();
 app.use(express.json());
 app.use(cors()); // Cho phép Live Server hoặc Vercel gọi API không bị chặn
 
